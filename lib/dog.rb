19bb664aea9d2ef6dcfb6628967e8ba1e0d86f7f
bkrigel <benjamin.krigel@gmail.com>
2018-05-18 11:26:16 -0400

    Still don't quite get the setter/getter methods

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+end
+
+
+
+class Person
+  def name=(full_name)
+    @name = full_name
+  end
+  def name
+    @name
+  end
+  def job=(job_title)
+    @job = job_title
+  end
+  def job
+    @job
+  end
+end
